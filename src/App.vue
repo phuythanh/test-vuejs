79f1b7db830d83a8863b512ed60838405a91c646
phuythanh <phuythanh@gmail.com>
2020-10-18 22:36:46 +0700

    test build

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img src="./assets/logo.png" />
     <el-container>
       <el-container>
-        <el-header>CORONAVIRUS COVID19 </el-header>
+        <el-header>CORONAVIRUS COVID20 </el-header>
         <el-main> <router-view></router-view></el-main>
       </el-container>
     </el-container>
